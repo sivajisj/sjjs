17ee966f6e585bfdf07e2d46ec3a6f7d24eb8ee7
Sivaji gadidala <93958018+sivajisj@users.noreply.github.com>
2025-09-24 14:05:13 +0000

    added anchor-function-tutorial project

---
+++ anchor-function-tutorial/programs/anchor-function-tutorial/src/lib.rs
@@ -0,0 +1,26 @@
+use anchor_lang::prelude::*;
+
+declare_id!("eDsiV96n5w7qsZPKjdqCudk5DGUgJnjYV7oPuGs72TJ");
+
+
+#[program]
+pub mod anchor_function_tutorial {
+    use super::*;
+
+    pub fn function_a(ctx: Context<NonEmptyAccountExample>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn function_b(ctx: Context<Empty>, firstArg: u64) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct NonEmptyAccountExample<'info> {
+    signer: Signer<'info>,
+    another_signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Empty {}
